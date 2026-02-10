001587dbf162c325d476c59b30ac8317c7a6dbd7
AITDOUCH-ELHOUCINE <elhoucineaitdouch00@gmail.com>
2026-02-10 11:43:35 +0100

    fix err logs write

--- config/env/production.js
+++ config/env/production.js
@@ -20,19 +20,18 @@ module.exports = {
     // Can specify one of 'combined', 'common', 'dev', 'short', 'tiny'
     format: process.env.LOG_FORMAT || 'combined',
     options: {
-      // Stream defaults to process.stdout
-      // Uncomment/comment to toggle the logging to a log on the file system
-      stream: {
-        directoryPath: process.env.LOG_DIR_PATH || resolve('logs'),
-        fileName: process.env.LOG_FILE || 'access.log',
-        rotatingLogs: {
-          // for more info on rotating logs - https://github.com/holidayextras/file-stream-rotator#usage
-          active: process.env.LOG_ROTATING_ACTIVE === 'true', // activate to use rotating logs
-          fileName: process.env.LOG_ROTATING_FILE || 'access-%DATE%.log', // if rotating logs are active, this fileName setting will be used
-          frequency: process.env.LOG_ROTATING_FREQUENCY || 'daily',
-          verbose: process.env.LOG_ROTATING_VERBOSE === 'true',
-        },
-      },
+      //      stream: {
+      //   directoryPath: process.env.LOG_DIR_PATH || resolve('logs'),
+      //   fileName: process.env.LOG_FILE || 'access.log',
+      //   rotatingLogs: {
+      //     // for more info on rotating logs - https://github.com/holidayextras/file-stream-rotator#usage
+      //     active: process.env.LOG_ROTATING_ACTIVE === 'true', // activate to use rotating logs
+      //     fileName: process.env.LOG_ROTATING_FILE || 'access-%DATE%.log', // if rotating logs are active, this fileName setting will be used
+      //     frequency: process.env.LOG_ROTATING_FREQUENCY || 'daily',
+      //     verbose: process.env.LOG_ROTATING_VERBOSE === 'true',
+      //   },
+      // },
+      stream: process.stdout,
     },
   },
   sockets: {
